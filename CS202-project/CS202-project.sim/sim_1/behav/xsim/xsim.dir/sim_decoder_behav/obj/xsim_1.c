e1c095d2b6ed34af02fc265d91d3dafb3c6813ff
jimmylaw21 <1908486736@qq.com>
2023-05-14 11:37:48 +0800

    ALU & decoder commit

---
+++ CS202-project/CS202-project.sim/sim_1/behav/xsim/xsim.dir/sim_decoder_behav/obj/xsim_1.c
@@ -0,0 +1,125 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_7(char*, char *);
+extern void execute_8(char*, char *);
+extern void execute_9(char*, char *);
+extern void execute_10(char*, char *);
+extern void execute_11(char*, char *);
+extern void execute_99(char*, char *);
+extern void execute_100(char*, char *);
+extern void execute_101(char*, char *);
+extern void execute_102(char*, char *);
+extern void execute_103(char*, char *);
+extern void execute_104(char*, char *);
+extern void execute_105(char*, char *);
+extern void execute_106(char*, char *);
+extern void execute_107(char*, char *);
+extern void execute_108(char*, char *);
+extern void execute_3(char*, char *);
+extern void execute_4(char*, char *);
+extern void execute_5(char*, char *);
+extern void execute_6(char*, char *);
+extern void execute_16(char*, char *);
+extern void execute_17(char*, char *);
+extern void execute_18(char*, char *);
+extern void execute_13(char*, char *);
+extern void execute_14(char*, char *);
+extern void execute_15(char*, char *);
+extern void execute_109(char*, char *);
+extern void execute_110(char*, char *);
+extern void execute_111(char*, char *);
+extern void execute_112(char*, char *);
+extern void execute_113(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[31] = {(funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_99, (funcp)execute_100, (funcp)execute_101, (funcp)execute_102, (funcp)execute_103, (funcp)execute_104, (funcp)execute_105, (funcp)execute_106, (funcp)execute_107, (funcp)execute_108, (funcp)execute_3, (funcp)execute_4, (funcp)execute_5, (funcp)execute_6, (funcp)execute_16, (funcp)execute_17, (funcp)execute_18, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_109, (funcp)execute_110, (funcp)execute_111, (funcp)execute_112, (funcp)execute_113, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 31;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/sim_decoder_behav/xsim.reloc",  (void **)funcTab, 31);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/sim_decoder_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/sim_decoder_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/sim_decoder_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/sim_decoder_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/sim_decoder_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
--- CS202-project/CS202-project.srcs/sources_1/new/Executs32.v
+++ CS202-project/CS202-project.srcs/sources_1/new/Executs32.v
@@ -1,34 +1,131 @@
 module Executs32(Read_data_1, Read_data_2, Sign_extend, 
-Opcode, Function_opcode, Shamt, PC_plus_4, 
-ALUOp, ALUSrc, I_format, Sftmd, ALU_Result, Zero, Addr_Result);
+Function_opcode, Exe_opcode, ALUOp, Shamt, ALUSrc, I_format, Zero,
+Jr, Sftmd, ALU_Result, Addr_Result, PC_plus_4);
 // from Decoder
 input[31:0] Read_data_1; //the source of Ainput
 input[31:0] Read_data_2; //one of the sources of Binput
 input[31:0] Sign_extend; //one of the sources of Binput
 // from IFetch
-input[5:0] Opcode; //instruction[31:26]
 input[5:0] Function_opcode; //instructions[5:0]
+input[5:0] Exe_opcode; //instruction[31:26]
 input[4:0] Shamt; //instruction[10:6], the amount of shift bits
 input[31:0] PC_plus_4; //pc+4
 // from Controller
 input[1:0] ALUOp; //{ (R_format || I_format) , (Branch || nBranch) }
 input ALUSrc; // 1 means the 2nd operand is an immediate (except beq,bne）
 input I_format; // 1 means I-Type instruction except beq, bne, LW, SW
+input Jr;   // from the control unit, indicating a JR instruction
 input Sftmd; // 1 means this is a shift instruction
 
-output[31:0] ALU_Result; // the ALU calculation result
+output reg[31:0] ALU_Result; // the ALU calculation result
 output Zero; // 1 means the ALU_reslut is zero, 0 otherwise
 output[31:0] Addr_Result; // the calculated instruction address
 
 wire[31:0] Ainput,Binput; // two operands for calculation
-wire[5:0] Exe_code; // use to generate ALU_ctrl. (I_format==0) ? Function_opcode : { 3'b000 , Opcode[2:0] };
+wire signed [31:0] Ainput_signed, Binput_signed; // two operands for calculation
+wire[5:0] Exe_code; // use to generate ALU_ctrl. (I_format==0) ? Function_opcode : { 3'b000 , Exe_opcode[2:0] };
 wire[2:0] ALU_ctl; // the control signals which affact operation in ALU directely
 wire[2:0] Sftm; // identify the types of shift instruction, equals to Function_opcode[2:0]
 reg[31:0] Shift_Result; // the result of shift operation
 reg[31:0] ALU_output_mux; // the result of arithmetic or logic calculation
 wire[32:0] Branch_Addr; // the calculated address of the instruction, Addr_Result is Branch_Addr[31:0]
 
-reg[31:0] ALU_Result_reg = 0;
-assign ALU_Result = ALU_Result_reg;
+// generate Ainput,Binput
+assign Ainput = Read_data_1;
+assign Binput = (ALUSrc == 0) ? Read_data_2 : Sign_extend[31:0];
+
+// generate Ainput_signed, Binput_signed
+assign Ainput_signed = Ainput;
+assign Binput_signed = Binput;
+
+// generate Exe_code
+assign Exe_code = (I_format == 0) ? Function_opcode : { 3'b000 , Exe_opcode[2:0] };
+
+// generate ALU_ctl
+assign ALU_ctl[0] = (Exe_code[0] | Exe_code[3]) & ALUOp[1];
+assign ALU_ctl[1] = ((!Exe_code[2]) | (!ALUOp[1]));
+assign ALU_ctl[2] = (Exe_code[1] & ALUOp[1]) | ALUOp[0];
+
+// generate Sftm
+assign Sftm = Function_opcode[2:0];
+
+always @* begin // six types of shift instructions
+    if(Sftmd)
+        case(Sftm[2:0])
+          3'b000: begin 
+            Shift_Result = Binput << Shamt; //Sll rd,rt,shamt 00000
+          end
+          3'b010: begin
+            Shift_Result = Binput >> Shamt; //Srl rd,rt,shamt 00010
+          end
+          3'b100: begin
+            Shift_Result = Binput << Ainput; //Sllv rd,rt,rs 00100
+          end
+          3'b110: begin
+            Shift_Result = Binput >> Ainput; //Srlv rd,rt,rs 00110
+          end
+          3'b011: begin
+            Shift_Result = Binput_signed >>> Shamt; // Sra rd,rt,shamt 00011
+          end
+          3'b111: begin
+            Shift_Result = Binput_signed >>> Ainput_signed; // Srav rd,rt,rs 00111
+          end
+          default: begin
+            Shift_Result = Binput;
+          end
+        endcase
+    else
+        Shift_Result = Binput;
+end
+
+// generate ALU_output_mux
+always @(ALU_ctl or Ainput or Binput)
+begin 
+    case(ALU_ctl[2:0])
+      //and, andi
+      3'b000:ALU_output_mux = Ainput & Binput;
+      //or, ori
+      3'b001:ALU_output_mux = Ainput | Binput;
+      //add, addi, lw, sw
+      3'b010:ALU_output_mux = Ainput_signed + Binput_signed;
+      //addu, addiu
+      3'b011:ALU_output_mux = Ainput + Binput;
+      //xor, xori
+      3'b100:ALU_output_mux = Ainput ^ Binput;
+      //nor, lui
+      3'b101:ALU_output_mux = ~(Ainput | Binput);
+      //sub, slti, beq, bne
+      3'b110:ALU_output_mux = Ainput_signed - Binput_signed;
+      //subu, sltiu, slt, sltu
+      3'b111:ALU_output_mux = Ainput - Binput;
+      default:ALU_output_mux = 0;
+    endcase
+end
+
+// generate ALU_Result
+always @* begin
+    //set type operation (slt, slti, sltu, sltiu)
+    if ( ((ALU_ctl == 3'b111) && (Exe_code[3] == 1)) || ((ALU_ctl[2:1] == 2'b11) && (I_format == 1)) )
+        ALU_Result = { 31'd0, ALU_output_mux[31] };
+    //lui operation
+    else if((ALU_ctl==3'b101) && (I_format==1))
+        ALU_Result[31:0] = {Binput[15:0], 16'd0};
+    //shift operation
+    else if(Sftmd==1)
+        ALU_Result = Shift_Result ;
+    //other types of operation in ALU (arithmatic or logic calculation)
+    else
+        ALU_Result = ALU_output_mux[31:0];
+end
+
+// generate Branch_Addr
+assign Branch_Addr = PC_plus_4 + (Sign_extend << 2);
+
+// generate Addr_Result
+assign Addr_Result = Branch_Addr[31:0];
+
+// generate Zero
+assign Zero = (ALU_output_mux[31:0] == 0) ? 1 : 0;
+
 
 endmodule
