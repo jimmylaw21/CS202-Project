5677dc97588bf0a7eae8adf4c99a32e4d319cc1f
sevenEki <101239523+LiaoChenyi@users.noreply.github.com>
2023-05-16 15:47:09 +0800

    Update seg.v

--- CS202-project/CS202-project.srcs/sources_1/new/seg.v
+++ CS202-project/CS202-project.srcs/sources_1/new/seg.v
@@ -21,45 +21,51 @@
 
 
 module seg(
-    input			segrst,		// reset, active high (��λ�ź�,�ߵ�ƽ��Ч)
-    input			seg_clk,	// clk for led (ʱ���ź�)
-    input			segwrite,	// led write enable, active high (д�ź�,�ߵ�ƽ��Ч)
-    input			segcs,		// 1 means the leds are selected as output (��memorio���ģ��ɵ�����λ�γɵ�LEDƬѡ�ź�)
-    input	[1:0]	ledaddr,	// 2'b00 means updata the low 16bits of ledout, 2'b10 means updata the high 8 bits of ledout
-    input	[15:0]	ledwdata,	// the data (from register/memorio)  waiting for to be writen to the leds of the board
+    input			segrst,		// reset, active high 
+    input			seg_clk,	// clk for led
+    input			segwrite,	// seg write enable, active high 
+    input			segcs,		// 1 means the segs are selected as output 
+    input	[15:0]	segwdata,	// the data (from register/memorio)  waiting for to be writen to the segs of the board
     output  [7:0]   enables,
-    output	reg [7:0] 	segout,		// the data writen to the leds  of the board
-    output          digitPoint
+    output	reg [6:0] 	segout		// the data writen to the segs  of the board
     );
     reg[7:0] lights;
     assign enables = lights;
     always @ (posedge seg_clk or posedge segrst) begin
         if (segrst) begin
-            lights <= 7'b000_0000;
+            lights <= 8'b1111_1111;
         end    
         else if (segcs && segwrite) begin
-            if (lights == 7'b000_0000 | lights == 7'b100_0000) begin
-                lights <= 7'b000_0001; 
+            if (lights == 8'b1111_1111 | lights == 8'b0111_1111) begin
+                lights <= 8'b1111_1110; 
             end 
             else begin
-                lights <= lights << 1;
+                lights <= ( lights << 1 | 8'b0000_0001);
             end        
         end        
-    end    
-    always @ (posedge seg_clk or posedge segrst) begin
+    end   
+    wire [3:0] groups [0:3];
+    assign groups[0] = segwdata [3:0];
+    assign groups[1] = segwdata [7:4];
+    assign groups[2] = segwdata [11:8];
+    assign groups[3] = segwdata [15:12];
+    wire [6:0] displays [0:3];
+    binaryToHex bth0(groups[0], displays[0]);
+    binaryToHex bth1(groups[1], displays[1]);
+    binaryToHex bth2(groups[2], displays[2]);
+    binaryToHex bth3(groups[3], displays[3]);
+    always @ (*) begin
         if (segrst) begin
-            segout <= 7'b000_0000;
-        end    
-		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-/*		else if (segcs && segwrite) begin
-			if (ledaddr == 2'b00)
-				ledout[23:0] <= { ledout[23:16], ledwdata[15:0] };
-			else if (ledaddr == 2'b10 )
-				ledout[23:0] <= { ledwdata[7:0], ledout[15:0] };
-			else
-				ledout <= ledout;
-        end else begin
-            ledout <= ledout;
-        end*/
-    end
+            segout = 7'b000_0000;
+        end
+        else begin
+            case (lights)
+                8'b1111_1110: segout = displays[0];
+                8'b1111_1101: segout = displays[1];
+                8'b1111_1011: segout = displays[2];
+                8'b1111_0111: segout = displays[3];
+                default: segout = 7'b100_0000;
+            endcase
+        end
+    end        
 endmodule
